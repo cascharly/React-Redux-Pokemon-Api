bd60f07d3c4294ed0268c6f192d8c1c727d03209
cascharly <cascharly@gmail.com>
2020-09-24 20:15:23 -0300

    Uso de Logger Middleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,7 @@
 import { createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 import { default as rootReducers } from "./reducers/rootReducers";
 
-const store = createStore(rootReducers);
+const store = createStore(rootReducers, composeWithDevTools());
 
 export default store;
