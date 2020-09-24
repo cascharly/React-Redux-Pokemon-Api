b89599d9834db5445b01f50476e2ad53b1656846
cascharly <cascharly@gmail.com>
2020-09-24 19:55:52 -0300

    Conectando Redux a React

--- src/App.js
+++ src/App.js
@@ -4,32 +4,38 @@ import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.bundle";
 import CantidadPokemon from "./components/CantidadPokemon";
 import CompraPokemon from "./components/CompraPokemon";
+import { Provider } from "react-redux";
 import store from "./redux/store";
 
 function App() {
-  console.log(store.getState())
   return (
-    <div className="App container">
-      <div className="row">
-        <div className="col-12">
-          <div className="card mt-5" style={{ maxWidth:'370px' }} >
-            <div className="row no-gutters">
-              <div className="col-4">
-                <img className='card-img' src={require('./img/pokemon-ball.jpg')} alt="pokemon"/>
-              </div>
-              <div className="col-8">
-                <div className="card-body">
-                  <div className="card-title h3 text-center">
-                    <CantidadPokemon />
+    <Provider store={store}>
+      <div className="App container">
+        <div className="row">
+          <div className="col-12">
+            <div className="card mt-5" style={{ maxWidth: "370px" }}>
+              <div className="row no-gutters">
+                <div className="col-4">
+                  <img
+                    className="card-img"
+                    src={require("./img/pokemon-ball.jpg")}
+                    alt="pokemon"
+                  />
+                </div>
+                <div className="col-8">
+                  <div className="card-body">
+                    <div className="card-title h3 text-center">
+                      <CantidadPokemon />
                     </div>
                     <CompraPokemon />
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </Provider>
   );
 }
 
